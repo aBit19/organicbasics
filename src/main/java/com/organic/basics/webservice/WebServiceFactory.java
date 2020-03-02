b70d22dbf0a2e19fec99a343daf078b0c9f5db12
abit19 <you@example.com>
2020-03-02 01:18:29 +0100

    Removes invalid token.

--- src/main/java/com/organic/basics/webservice/WebServiceFactory.java
+++ src/main/java/com/organic/basics/webservice/WebServiceFactory.java
@@ -1,16 +1,12 @@
 package com.organic.basics.webservice;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
 
 public class WebServiceFactory {
 
-  private static final String TOKEN = "bbf0cb12afd3c2a2fb997a61188408ce1277dd30";
-  private static final String USERNAME = "aBit19";
-
-  private WebServiceFactory() {
-  }
+  private WebServiceFactory() {}
 
   public static <T> WebService<T> getInstance(Class<T> type) {
-    return new WebServiceImpl<>(type, new RestTemplateBuilder().basicAuthentication(USERNAME, TOKEN).build());
+    return new WebServiceImpl<>(type, new RestTemplate());
   }
 }
